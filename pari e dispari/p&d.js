6004d7248bffa7d54aa9d5379a08c839e7bbe14f
Giulia-92 <cionco.giulia@gmail.com>
2022-04-04 10:01:49 +0200

    js-paliedispari

---
+++ pari e dispari/p&d.js
@@ -0,0 +1,15 @@
+let gioco = prompt ("scrivi 1 per dispari e 0 per pari");
+let gioco1 = prompt("inserisci un valore da 1 a 5");
+let utente ='';
+let valore1= 1;
+let valore2 = 0;
+myfunction() {
+return (math.floor(math.random)*5)+1};
+let computer = myfunction();
+let somma = utente + computer;
+function pariDispari(somma){
+if (somma % 2 == 00)
+return valore1,valore2}
+if (pariDispari == valore1,valore2){
+prompt("hai vinto");
+} else { prompt ("hai perso")}
---
+++ parole/script.js
@@ -0,0 +1,8 @@
+let parola = "Kayak";
+let lparola = parola.legth();
+let lparola1 = '';
+while (lparola > lparola1)
+if (lparola[i] == lparola[i]){
+}else{
+alert("parola non palindroma")
+}
